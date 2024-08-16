8aa3d3f13657068d27aa113c5362c789a89715ef
Hedgehawk11 <84346970+Hedgehawk11@users.noreply.github.com>
2024-08-16 17:56:46 +0000

    the start

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-input('DISCLAMER: This game was made to be terrible, it is played in the command line, does NOT adapt to your input(for the most part) and you will probably get a lot of errors')
+input('DISCLAMER: This game was made to be terrible, it is played in the command line, has 1 variable and you will probably get a lot of errors'"\n")
 import sys as sus
 import time as t
-
+import clear as c
+c.clear()
+input('what is your name')
+print('I actually don\'t care, your name is John now. \n')
+t.sleep(5)
+input('you\'re stil here? ')
+c.clear()
+t.sleep(10)
+input('usually people are gone by now ')
+c.clear()
+t.sleep(5)
+var = input('you are going to wait a whole minute')
+if (var == 'ok' or var == 'yes' or var == 'y'):
+    print(var + '?','IT\'s A LITERAL MINUTE AND YOU\'re OK WITH IT')
+    t.sleep(3)
+    print('ok, fine. Here you go, geez')
+t.sleep(60)
+c.clear()
+input('you do know I don\'t care about your responces ')
+print('not even that one')
