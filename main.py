9ef0a3b0ce5f74c910242ead9b6802f9b4af8d50
Hedgehawk11 <84346970+Hedgehawk11@users.noreply.github.com>
2024-08-16 17:38:06 +0000

    DISCLAMER

---
+++ main.py
@@ -0,0 +1,4 @@
+input('DISCLAMER: This game was made to be terrible, it is played in the command line, does NOT adapt to your input(for the most part) and you will probably get a lot of errors')
+import sys as sus
+import time as t
+
